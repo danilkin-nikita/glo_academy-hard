81e9d6fd3a5cc9054a84c22b5bb6e934906eeeb5
Nikita Danilkin <ni.danilkin@gmail.com>
2020-12-27 20:50:10 +0300

    lesson03

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
 'use strict';
 
-let num = 266219;
+let arr = ['23', '2', '65', '40', '471', '90', '30'];
 
-let calculation = (num.toString().split('').reduce((res, item) => item * res, 1)) ** 3;
-console.log(calculation);
+arr.forEach((item) => {
+  if (item.startsWith(2) || item.startsWith(4)) {
+    console.log(item);
+  }
+});
+
+function isPrime(n) {
+  for (let i = 2; i * i <= n; i == 2 ? i++ : i += 2) if (n % i == 0) {
+    return false;
+  } 
+  return n;
+}
+
+const res = [...Array(100)].reduce((a, _, i) => a.concat(isPrime(i) ? `Делители числа ${i}: 1 и ${i}` : []) , []).join('\n');
+
+console.log(res);
