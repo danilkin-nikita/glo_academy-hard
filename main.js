f37909f8c48ae14f69f74963bca460ecabf035aa
Nikita Danilkin <ni.danilkin@gmail.com>
2020-12-27 21:18:57 +0300

    lesson07

--- main.js
+++ main.js
@@ -1,20 +1,19 @@
 'use strict';
 
-let arr = ['23', '2', '65', '40', '471', '90', '30'];
+const daysOfTheWeek = () => {
+  let day = document.querySelectorAll(".day");
 
-arr.forEach((item) => {
-  if (item.startsWith(2) || item.startsWith(4)) {
-    console.log(item);
+  let daysArr = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+
+  for (let i = 0; i < daysArr.length; i++) {
+    day[i].textContent = daysArr[i];
+    if (i == 5 || i == 6) {
+      day[i].style.fontStyle = 'italic';
+    }
   }
-});
+  
+  let today = (6 + new Date().getDay()) % 7;
+  day[today].style.fontWeight = 'bold';
+}; 
 
-function isPrime(n) {
-  for (let i = 2; i * i <= n; i == 2 ? i++ : i += 2) if (n % i == 0) {
-    return false;
-  } 
-  return n;
-}
-
-const res = [...Array(100)].reduce((a, _, i) => a.concat(isPrime(i) ? `Делители числа ${i}: 1 и ${i}` : []) , []).join('\n');
-
-console.log(res);
+daysOfTheWeek();
